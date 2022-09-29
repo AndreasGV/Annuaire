02bf81328f54258fd2a3d003919ef23b74e249d6
AndreasGV <113093550+AndreasGV@users.noreply.github.com>
2022-09-29 08:42:38 +0200

    Add files via upload
    First commit

---
+++ annuaire-VerEtudt/controllers/c_gerer.php
@@ -0,0 +1,41 @@
+<?php
+switch ($action) {
+    case 'accueil':
+    {
+        $message="Ce site permet d'enregistrer les participants à une épreuve.";
+        include("views/v_accueil.php");
+        break;
+    }
+    case 'lister': {
+        $les_membres=$pdo->getLesMembres();
+        require 'views/v_listemembres.php';
+        break;
+    }
+    case 'saisir':
+    {
+        require "views/v_saisie.php";
+        break;
+    }
+    case 'controlesaisie':
+    {
+        $nom = $_REQUEST['nom'];
+        $prenom = $_REQUEST['prenom'];
+
+        if (empty($nom) || empty($prenom)) {
+            require "views/v_saisie.php";
+        } else {
+            $resultat = $pdo->insertMembre($nom, $prenom);
+            $message='Vous avez saisie un membre';
+            include("views/v_accueil.php");
+        }
+        break;
+    }
+    default:
+    {
+        $_SESSION["message_erreur"] = "Site en construction";
+        include("views/404.php");
+        break;
+    }
+
+
+}
---
+++ annuaire-VerEtudt/views/v_saisie.php
@@ -0,0 +1,14 @@
+
+</br>
+
+<form action="index.php?uc=gerer&action=controlesaisie" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom">
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom">
+
+        <input type="submit" id="saisir" name="saisir">
+        
+    </div>
+</form>
